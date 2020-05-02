19c22c815398fedf447b3ec98b430651e47a4e0e
vasilijus <vasiliokas@gmail.com>
2020-05-02 21:38:23 +0100

    implement PersonDao, and then implement it's methods ( Alt+Enter ) shortcode

--- src/main/java/com/sergej/springtut/dao/PersonDataAccessService.java
+++ src/main/java/com/sergej/springtut/dao/PersonDataAccessService.java
@@ -1,4 +1,34 @@
 package com.sergej.springtut.dao;
 
-public class PersonDataAccessService {
+import com.sergej.springtut.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PersonDataAccessService implements PersonDao {
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return null;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
 }
